9ea0340de78390bfb453beee61703986fb332dae
Stella Christou <stellachristou@Stellas-MacBook-Air-2.local>
2025-12-10 12:11:52 +0100

    hii8

--- stella/stella script.R
+++ stella/stella script.R
@@ -1,1 +1,11 @@
 stella skript
+# schritt 1 ist packages laden
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("janitor")
+install.packages("skimr")
+install.packages("broom")
+install.packages("ggplot2")     # meist in tidyverse drin, aber zur Sicherheit
+install.packages("readr")       # auch im tidyverse, aber gut zum Nachinstallieren
+install.packages("dplyr")       # ebenfalls tidyverse
+install.packages("forcats")     # für Kategorien
